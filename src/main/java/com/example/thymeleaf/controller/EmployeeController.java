fe98474c9e22ea69ced7d121ec8f9861dcb114ec
17KryptoVos <17kryptovos@protonmail.com>
2020-08-16 19:54:49 +0200

    Used @PostConstruct

--- src/main/java/com/example/thymeleaf/controller/EmployeeController.java
+++ src/main/java/com/example/thymeleaf/controller/EmployeeController.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,18 +14,25 @@ import java.util.List;
 @RequestMapping("employees")
 public class EmployeeController {
 
-    @GetMapping("/list")
-    public String listEmployees(Model theModel) {
+    private List<Employee> employeeList;
+
+    @PostConstruct
+    private void loadData(){
         // Create employees
         Employee emp1 = new Employee(1, "Peter", "Vaughn", "peter@blabla.com");
         Employee emp2 = new Employee(1, "Juan", "Vaughn", "juan@blabla.com");
         Employee emp3 = new Employee(1, "Marie", "Vaughn", "marie@blabla.com");
 
-        List<Employee> employeeList = new ArrayList<>();
+        employeeList = new ArrayList<>();
 
         employeeList.add(emp1);
         employeeList.add(emp2);
         employeeList.add(emp3);
+    }
+
+    @GetMapping("/list")
+    public String listEmployees(Model theModel) {
+
 
         theModel.addAttribute("employeeList", employeeList);
         return "list-employees";
